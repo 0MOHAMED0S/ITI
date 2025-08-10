ac6224d4f0ab6370bc3123f077df8dd62f526d7a
mohamed <msa0back@gmail.com>
2025-08-10 19:57:02 +0300

    jh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,17 @@
                         >
                             Dashboard
                         </a>
+
+                        <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-responsive-nav-link>
+                </form>
                     @else
                         <a
                             href="{{ route('login') }}"
